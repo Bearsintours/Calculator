6a11d42f56f3ae21c937032af9219f6048cb13bb
Quentin <qbrogly@gmail.com>
2018-03-05 21:25:01 -0800

    Fixed numbers overflowing screen issue

--- data.js
+++ data.js
@@ -20,14 +20,22 @@ $(document).ready(function() {
       //If it is a new calculation: empty screen and store
       if (newCalcul == true) {
         screen.text("");
+        screen.removeClass("smaller").addClass("regular");
         store = [];
         newCalcul = false;
+      }
+      // Limit number of digits calculator can take
+      if (store.length > 20) {
+        newCalcul = true;
+      }
+      // Change font-size if more than 10 digits on screen
+      if (store.length > 10) {
+        screen.removeClass("regular").addClass("smaller");
       }
       // store the number and print it on screen
       var val = e.target.textContent;
       store.push(val);
       screen.append(val);
-
     })
     //When click operation (+, -, *, or /) :
     $(".operation").click(function(e) {
@@ -37,28 +45,18 @@ $(document).ready(function() {
       screen.append(val);
       newCalcul = false;
     })
-
     //When click " = " :
     $("#equal").click(function(e) {
       //Use eval() to get result, store it and print it
-      var printResult = store.join("");
-      screen.text(eval(printResult).toFixed(2));
-      var result = eval(printResult);
+      var result = store.join("");
+      result = eval(result);
+      result.toString().length > 12
+        ? screen.text(result.toFixed(10))
+        : screen.text(result);
       store = [result];
-      //Set newCalcul to true so if next button clicked is a number, it will reset screen and start a new calculation
       newCalcul = true;
     })
   }
-
-  // var error = function(){
-  //   $("#number, #operation").click(function(e){
-  //     if(store.length > 12){
-  //       screen.text("error");
-  //       store=[];
-  //       newCalcul = true;
-  //     }
-  //   })
-  // }
 
   init();
   calculate();
